086e221ef65f0855e49eb93b0d3756e5271a8300
johnjkim209 <johnjkim209@gmail.com>
2026-02-10 20:25:50 -0500

    scanner stuff

--- Exercise3/Gradebook.java
+++ Exercise3/Gradebook.java
@@ -1,5 +1,23 @@
-public class assignment1 {
-    public static void main(String[] args) {
-        System.out.println("Hello World! Please work");
+import java.util.Scanner;
+
+class Gradebook {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.print("Divide 10 by: ");
+            int divisor = scanner.nextInt();
+            int result = 10 / divisor;
+            System.out.println("Result: " + result);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a valid integer.");
+        } catch (ArithmeticException e) {
+            System.out.println("Division by 0 not allowed. Try again.");
+        } finally {
+            scanner.close();
+        }
+
+        System.out.println("Congrats u did it");
+        System.out.println("end of program");
     }
 }
