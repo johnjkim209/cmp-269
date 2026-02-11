5cdc8145726bf6a755f21885b8eba0f61cae88e6
johnjkim209 <johnjkim209@gmail.com>
2026-02-10 20:58:32 -0500

    file scanner

--- Exercise3/Gradebook.java
+++ Exercise3/Gradebook.java
@@ -1,8 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 class Gradebook {
     public static void main(String args[]) {
+        String file_input = "students.txt";
+        String file_output = "grades_report.txt";
+        File file = new File(file_input);
+        try (Scanner scanner = new Scanner(file)) {
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("file not found");
+            e.printStackTrace();
+        }
+
+        /*
         Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        while (!line.equals("")) {
+            System.out.println(line);
+        }
+
+
         try {
             System.out.print("Divide 10 by: ");
             int divisor = scanner.nextInt();
@@ -19,5 +41,6 @@ class Gradebook {
 
         System.out.println("Congrats u did it");
         System.out.println("end of program");
+         */
     }
 }
