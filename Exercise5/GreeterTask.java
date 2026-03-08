f85a0d1d3c8e5f5ea90cd3c953fccd227b969daf
johnjkim209 <johnjkim209@gmail.com>
2026-03-08 07:32:17 -0400

    done with e5.1 and e5.2

--- Exercise5/GreeterTask.java
+++ Exercise5/GreeterTask.java
@@ -1,7 +1,26 @@
 public class GreeterTask implements Runnable {
     @Override
     public void run() {
-        // TODO Auto-generated method stub
+        try {
+            for(int i=0;i<5;i++) {
+                System.out.println("Hello from " + Thread.currentThread().getName());
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Thread was interrupted. ");
+            System.out.println(e.getMessage());
+            Thread.currentThread().interrupt();  // restore interrupt state?
+        }
         
+    }
+
+    public static void main(String[] args) {
+        GreeterTask greeterTask = new GreeterTask();
+
+        Thread thread1 = new Thread(greeterTask);
+        Thread thread2 = new Thread(greeterTask);
+
+        thread1.start();
+        thread2.start();
     }
 }
