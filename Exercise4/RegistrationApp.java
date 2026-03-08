12b0737d97d825c049feda4d92a0e9da51ba98ca
johnjkim209 <johnjkim209@gmail.com>
2026-03-08 06:52:36 -0400

    done

--- Exercise4/RegistrationApp.java
+++ Exercise4/RegistrationApp.java
@@ -9,6 +9,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.text.Font;
@@ -16,26 +17,53 @@ import javafx.scene.text.Font;
 public class RegistrationApp extends Application {
     @Override
     public void start(Stage primaryStage) {
+        VBox vbox = new VBox();
+        vbox.setPadding(new Insets(20));
+        vbox.setSpacing(10);
+
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(20));
         grid.setVgap(10);
         grid.setHgap(10);
 
-
-        Label label = new Label("hello world");
-        label.setFont(new Font("Arial", 24));
+        Label label = new Label("Student Name");
+        label.setFont(new Font("Arial", 16));
         label.setTextFill(Color.BLACK);
 
+        TextField inputName = new TextField();
+        inputName.setPrefWidth(200);
+
+        Label label2 = new Label("Course Code");
+        label2.setFont(new Font("Arial", 16));
+        label2.setTextFill(Color.BLACK);
+
+        ComboBox<String> comboBox = new ComboBox<>();
+        comboBox.getItems().addAll("CMP 269", "CMP 334", "CMP 338");
+        comboBox.setPrefWidth(200);
+
         grid.add(label, 0, 0);
+        grid.add(inputName, 1, 0);
+        grid.add(label2, 0, 1);
+        grid.add(comboBox, 1, 1);
 
-        // TODO: Create Labels and TextFields
-        // TODO: Create the Register Button
-        // TODO: Implement the Button Action using a Lambda
+        Button register = new Button("Register!");
 
+        Label status = new Label("Status: ");
+        status.setFont(new Font("Arial", 16));
+        status.setTextFill(Color.BLACK);
 
+        register.setOnAction(event -> {
+            String name = inputName.getText();
+            String course = comboBox.getValue();
+            status.setText("Registration successful for " + name + " in " + course + "!");
 
+        });
 
-        Scene scene = new Scene(grid, 400, 250);
+        vbox.getChildren().add(grid);
+        vbox.getChildren().add(register);
+        vbox.getChildren().add(status);
+
+        Scene scene = new Scene(vbox, 400, 250);
         primaryStage.setTitle("Lehman Course Registration");
         primaryStage.setScene(scene);
         primaryStage.show();
