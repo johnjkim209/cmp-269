424a4ba0e6b2ff733d4a4d4eec4a4c4f67abaa3d
johnjkim209 <johnjkim209@gmail.com>
2026-03-08 04:28:26 -0400

    hello world

--- Exercise4/RegistrationApp.java
+++ Exercise4/RegistrationApp.java
@@ -9,7 +9,9 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.text.Font;
 
 public class RegistrationApp extends Application {
     @Override
@@ -19,9 +21,19 @@ public class RegistrationApp extends Application {
         grid.setVgap(10);
         grid.setHgap(10);
 
+
+        Label label = new Label("hello world");
+        label.setFont(new Font("Arial", 24));
+        label.setTextFill(Color.BLACK);
+
+        grid.add(label, 0, 0);
+
         // TODO: Create Labels and TextFields
         // TODO: Create the Register Button
         // TODO: Implement the Button Action using a Lambda
+
+
+
 
         Scene scene = new Scene(grid, 400, 250);
         primaryStage.setTitle("Lehman Course Registration");
